ccbd07f4b6c4c28ef57c1e599754567acda350b4
sumanb14 <sumanbshetty07@gmail.com>
2023-09-05 13:42:59 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/myquiz/question.java
@@ -0,0 +1,24 @@
+package com.example.myquiz;
+
+public class question {
+    public static String question[] ={
+            "Which city is known as Silicon valley of India?",
+            "Which is India's largest city BY population?",
+            "Who is the Prime minister of India?",
+            "Which is the largest river in Karnataka?"
+    };
+
+    public static String choices[][] = {
+            {"Mumbai","Chennai","Bengaluru","Hyderabad"},
+            {"Bengaluru","Hyderabad","Mumbai","Chennai"},
+            {"Nehru","Manmohan Singh","Rajiv Gandhi","Narendra Modi"},
+            {"Sharavathi","Kaveri","Krishna","Tunga"}
+    };
+
+    public static String correctAnswers[] = {
+            "Bengaluru",
+            "Mumbai",
+            "Narendra Modi",
+            "Kaveri"
+    };
+}
